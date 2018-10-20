e5cb43e5a27426404d6bcd90d240acf11b6d002f
DESKTOP-G76VKCN\kacpe <kakola3@o2.pl>
2018-10-20 18:27:36 +0200

    Update Lesson 51 Bean LifeCycle

--- Spring tutorial/spring-demo-one/com/luv2code/springdemo/TrackCoach.java
+++ Spring tutorial/spring-demo-one/com/luv2code/springdemo/TrackCoach.java
@@ -21,4 +21,13 @@ public class TrackCoach implements Coach {
     public String getDailyFortune() {
         return "TrackCoach is your lucky day!";
     }
+
+    // add an init method
+    public void doMyStartupStuff(){
+        System.out.println("TrackCoach: inside method doMyStartupStuff");
+    }
+    // add a destroy method
+    public void doMyCleanupStuffYoYo(){
+        System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");
+    }
 }
