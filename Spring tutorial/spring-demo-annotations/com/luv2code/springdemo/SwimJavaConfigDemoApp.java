715887e6e94dcb1591f33a7c13d788e8e318d800
DESKTOP-G76VKCN\kacpe <kakola3@o2.pl>
2018-10-23 20:38:31 +0200

    Update lesson 95 Injecting Values from Properties File

---
+++ Spring tutorial/spring-demo-annotations/com/luv2code/springdemo/SwimJavaConfigDemoApp.java
@@ -0,0 +1,31 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SwimJavaConfigDemoApp
+{
+    public static void main(String[] args) {
+        // read spring java configuration class
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(SportConfig.class);
+        // get the bean from spring container
+        Coach theCoach = context.getBean("swimCoach", SwimCoach.class);
+        // call a method on a bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        // Practice activity 4 - get the bean from spring container
+        //Coach handCoach = context.getBean("handCoach", Coach.class);
+        // Practice activity 4 - call method on a bean
+        //System.out.println(handCoach.getDailyWorkout());
+
+        // call method to get the daily fortune
+        System.out.println(theCoach.getDailyFortune());
+
+        // call our new swim coach methods ... has the props values injected
+        System.out.println("email: " + ((SwimCoach) theCoach).getEmail());
+        System.out.println("team: " + ((SwimCoach) theCoach).getTeam());
+
+        // close the context
+        context.close();
+    }
+}
--- Spring tutorial/spring-demo-annotations/com/luv2code/springdemo/TennisCoach.java
+++ Spring tutorial/spring-demo-annotations/com/luv2code/springdemo/TennisCoach.java
@@ -2,21 +2,34 @@ package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach
 {
     @Autowired
-    @Qualifier("dataBaseFortuneService")
+    @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
     // define a default constructor
 
     public TennisCoach(){
         System.out.println(">> TennisCoach: inside default constructor");
+    }
+
+    // define my init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println(">> TennisCoach: inside of doMyStartupStuff()");
+    }
+
+    // define my destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");
     }
 
     // define a setter method
